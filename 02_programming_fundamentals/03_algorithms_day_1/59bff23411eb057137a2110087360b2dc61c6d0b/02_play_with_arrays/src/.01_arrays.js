2f361d04683e8f5602e774e39bea74069a5df59e
ricdnt <richard.donat@yahoo.fr>
2020-09-10 14:21:14 +0200

    end install asdf

---
+++ 02_programming_fundamentals/03_algorithms_day_1/59bff23411eb057137a2110087360b2dc61c6d0b/02_play_with_arrays/src/.01_arrays.js
@@ -0,0 +1,6 @@
+/*Create an array called `digits` with numbers **from 0 to 9**;
+- Create a constant called `first` for the **first slot** of your array;
+- Create a constant called `last` for the **last slot** of your array;
+- Create a constant called `sixth` for the slot at **the position 6** of your array;*/
+
+const digits = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
