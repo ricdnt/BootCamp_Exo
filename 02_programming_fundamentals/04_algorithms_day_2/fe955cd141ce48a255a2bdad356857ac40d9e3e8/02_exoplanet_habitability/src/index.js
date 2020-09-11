c555a63212664a57f38923e3f573c707f3ae00ce
ricdnt <richard.donat@yahoo.fr>
2020-09-11 11:15:23 +0200

    dowload today

---
+++ 02_programming_fundamentals/04_algorithms_day_2/fe955cd141ce48a255a2bdad356857ac40d9e3e8/02_exoplanet_habitability/src/index.js
@@ -0,0 +1,15 @@
+const star = { spectralClass: "M" };
+
+const planet = {
+  mass: 1.02,
+  radius: 1.12,
+  rotationStability: true,
+  habitalZone: false,
+};
+
+function canHabitateLife(star, planet) {
+  // Code the function here.
+}
+
+// Do not remove last lines, it is for tests
+module.exports = canHabitateLife;
